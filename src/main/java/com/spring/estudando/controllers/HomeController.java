169a0da8b1dd2f60f6230f47126ba49228470e78
Lucas Duarte <lucasduartedev@gmail.com>
2021-07-10 08:33:35 -0300

    Aplicando o primeiro controller

---
+++ src/main/java/com/spring/estudando/controllers/HomeController.java
@@ -0,0 +1,14 @@
+package com.spring.estudando.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/home")
+	public String home() {
+		return "<h1>Página Home</h1>";
+	}
+
+}
